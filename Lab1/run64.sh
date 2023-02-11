ac1ae8094bc19ca2b5a86cef309701cbc02d7e49
mike3090 <mike3090@163.com>
2023-02-11 23:14:58 +0800

    updated 64-bit version of exercise 5

---
+++ Lab1/run64.sh
@@ -0,0 +1,2 @@
+gcc -m64 size.c -Wpedantic -Wall -Wextra -Werror -std=c89 -o size
+./size
---
+++ Lab1/size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+
+#define PS(x) printf("Size of %s: %lu\n",#x,(long unsigned int)sizeof(x))
+
+int main(){
+    PS(char);
+    PS(short);
+    PS(short int);
+    PS(int);
+    PS(long int);
+    PS(unsigned int);
+    PS(void *);
+    PS(size_t);
+    PS(float);
+    PS(double);
+    PS(int8_t);
+    PS(int16_t);
+    PS(int32_t);
+    PS(int64_t);
+    PS(time_t);
+    PS(clock_t);
+    PS(struct tm);
+    PS(NULL);
+
+
+    return 0;
+}
